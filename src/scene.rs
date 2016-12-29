f9522ec0f69fdbf58b7ca453e44accf8c7d5645a
R.Chavignat <r.chavignat@gmail.com>
2016-12-30 00:45:45 +0100

    Add render method to scene

--- src/scene.rs
+++ src/scene.rs
@@ -1,4 +1,4 @@
-use image::Rgb;
+use image::{Rgb, RgbImage};
 
 pub struct Scene {
     bg: Rgb<u8>,
@@ -16,6 +16,14 @@ impl Scene {
     pub fn set_background(&mut self, background: Rgb<u8>) {
         self.bg = background;
     }
+
+    pub fn render(&self, width: u32, height: u32) -> RgbImage {
+        let mut img = RgbImage::new(width, height);
+        for pix in img.pixels_mut() {
+            *pix = self.bg;
+        }
+        img
+    }
 }
 
 #[cfg(test)]
@@ -27,5 +35,15 @@ mod tests {
         let c = Rgb { data: [90, 90, 90] };
         let scene = Scene::new(c);
         assert!(scene.background() == c);
+    }
+
+    #[test]
+    fn test_scene_render() {
+        let c = Rgb { data: [90, 90, 90] };
+        let scene = Scene::new(c);
+        let rendered_img = scene.render(400, 300);
+        for pixel in rendered_img.pixels() {
+            assert!(*pixel == c);
+        }
     }
 }
