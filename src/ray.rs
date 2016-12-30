0528e9345000665f15e182212709be58cecb157a
R.Chavignat <r.chavignat@gmail.com>
2016-12-30 01:33:49 +0100

    Remove Ray's type parameter

--- src/ray.rs
+++ src/ray.rs
@@ -1,12 +1,12 @@
 use nalgebra::{Vector3, Point3};
 
-pub struct Ray<N> {
-    pub origin: Point3<N>,
-    pub direction: Vector3<N>
+pub struct Ray {
+    pub origin: Point3<f64>,
+    pub direction: Vector3<f64>
 }
 
-impl<N> Ray<N> {
-    pub fn new(origin: Point3<N>, direction: Vector3<N>) -> Ray<N> {
+impl Ray {
+    pub fn new(origin: Point3<f64>, direction: Vector3<f64>) -> Ray {
         Ray { origin: origin, direction: direction}
     }
 }
@@ -17,8 +17,8 @@ mod tests {
 
     #[test]
     fn test_new_ray() {
-        let origin = Point3::new(0, 0, 0);
-        let direction = Vector3::new(0, 0, 1);
+        let origin = Point3::new(0., 0., 0.);
+        let direction = Vector3::new(0., 0., 1.);
         let ray = Ray::new(origin, direction);
         assert!(ray.direction == direction);
         assert!(ray.origin == origin);
