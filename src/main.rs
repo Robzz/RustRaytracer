7dac969dbfb321ecafdcdcce38e050bf0a0cc769
R.Chavignat <r.chavignat@gmail.com>
2016-12-30 01:34:23 +0100

    Command line arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+#![feature(plugin)]
+#![plugin(docopt_macros)]
+
+extern crate docopt;
 extern crate image;
 extern crate nalgebra;
+extern crate rustc_serialize;
 
 mod ray;
 mod face;
@@ -11,9 +16,22 @@ use image::Rgb;
 use std::path::Path;
 use scene::Scene;
 
+docopt!(Args, "
+Usage: raytrace <output> <width> <height>
+
+Options:
+    -a, --archive  Copy everything.
+",
+arg_width: u32, arg_height: u32);
+
 fn main() {
-    let output_path = Path::new("render.png");
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    let width = args.arg_width;
+    let height = args.arg_height;
+    let output = args.arg_output;
+
+    let output_path = Path::new(&output);
     let scene = Scene::new(Rgb { data: [90, 90, 90] });
-    let render = scene.render(400, 300);
+    let render = scene.render(width, height);
     render.save(output_path).expect("Cannot save output image");
 }
